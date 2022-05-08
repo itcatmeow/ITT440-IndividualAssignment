563e28ea76f03298441bf783fec6c9803a7c2f1e
itcatmeow <2020461282@isiswa.uitm.edu.my>
2022-05-08 12:00:31 +0000

    TCP

--- tcpclient.c
+++ tcpclient.c
@@ -1,63 +1,40 @@
-#include <netdb.h>
+#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
-void func(int sockfd)
+#include <arpa/inet.h>
+#include <time.h>
+
+#define MAX_BUFFER 128
+#define DAYTIME_SERVER_PORT 13
+
+int main ()
 {
-    char buff[MAX];
-    int n;
-    for (;;) {
-        bzero(buff, sizeof(buff));
-        printf("Enter the string : ");
-        n = 0;
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-        write(sockfd, buff, sizeof(buff));
-        bzero(buff, sizeof(buff));
-        read(sockfd, buff, sizeof(buff));
-        printf("From Server : %s", buff);
-        if ((strncmp(buff, "exit", 4)) == 0) {
-            printf("Client Exit...\n");
-            break;
+        int connectionFd, in, index = 0, limit = MAX_BUFFER;
+        struct sockaddr_in servaddr;
+        char timebuffer[MAX_BUFFER+1];
+
+        connectionFd = socket(AF_INET, SOCK_STREAM, 0);
+
+        memset(&servaddr, 0, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(DAYTIME_SERVER_PORT);
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+
+        connect(connectionFd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+        while((in=read(connectionFd, &timebuffer[index], limit))> 0) {
+
+                index += in;
+		limit -= in;
         }
-    }
+	
+	timebuffer[index] = 0;
+	printf("\n%s\n", timebuffer);
+
+	close(connectionFd);
+
+	return(0);
 }
-   
-int main()
-{
-    int sockfd, connfd;
-    struct sockaddr_in servaddr, cli;
-   
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
-   
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    servaddr.sin_port = htons(PORT);
-   
-    // connect the client socket to server socket
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
-        printf("connection with the server failed...\n");
-        exit(0);
-    }
-    else
-        printf("connected to the server..\n");
-   
-    // function for chat
-    func(sockfd);
-   
-    // close the socket
-    close(sockfd);
-}
+
