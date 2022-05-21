bb31088b6f02761a677499397ce778a2b92835f1
itcatmeow <2020461282@isiswa.uitm.edu.my>
2022-05-21 15:04:09 +0000

    Fixing Client and server

--- tcpserver.c
+++ tcpserver.c
@@ -10,21 +10,38 @@
 
 int main (void) 
 {
-	int serverFd, connectionFd;
+	int serverFd, connectionFd, socket_desc, new_socket, c;
 	struct sockaddr_in servaddr;
 	char timebuffer[MAX_BUFFER+1];
 	time_t currentTime;
 	
-	serverFd = socket(AF_INET, SOCK_STREAM, 0);
+	//Create socket
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_desc == -1)
+	{
+		printf("Could not create socket");
+	}
 
+	//Prepare the sockaddr_in structure
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(DAYTIME_SERVER_PORT);
 
-	bind(serverFd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-	listen(serverFd, 5);
+	//Bind
+	if(bind(socket_desc, (struct sockaddr *)&servaddr, sizeof(servaddr))<0)
+	{
+		puts("Bind failed");
+		return 1;
+	}
 
+	puts("Bind done");
+	
+	//Listen
+	listen(serverFd, 5);
+	
+	//Accept and incoming connectionns...
+	puts("Waiting for incoming connections...");
 	while(1) {
 	
 		connectionFd = accept(serverFd, (struct sockaddr *)NULL, NULL);
